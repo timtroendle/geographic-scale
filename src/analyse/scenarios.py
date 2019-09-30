6afebb92075b8728291638145fd0907fce46cb13
Tim Tröndle <tim.troendle@usys.ethz.ch>
2019-09-30 12:36:13 +0200

    Update axis descriptions of cost overview

--- src/analyse/scenarios.py
+++ src/analyse/scenarios.py
@@ -9,6 +9,9 @@ import xarray as xr
 
 GREEN = "#679436"
 RED = "#A01914"
+YELLOW = "#FABC3C"
+SYSTEM_SCALE_COLOR = "k"
+AUTARKY_EXTENT_COLOR = "k"
 HIGHLIGHT_COLORS = sns.light_palette(GREEN, n_colors=4, reverse=False)[1:]
 HIGHLIGHT_LINEWIDTH = 4
 PANEL_FONT_SIZE = 10
@@ -16,24 +19,24 @@ PANEL_FONT_WEIGHT = "bold"
 
 BASE_SCENARIO = "continental-autarky-100-continental-grid"
 DATA_INDEX = """autarky_layer,grid_scale,autarky_degree,cost
-Regional,Regional,100%,
-Regional,National,100%,
-Regional,Continental,100%,
-Regional,National,≥85%,
-Regional,Continental,≥85%,
-Regional,National,≥70%,
-Regional,Continental,≥70%,
-National,National,100%,
-National,Continental,100%,
-National,Continental,≥85%,
-National,Continental,≥70%,
-Continental,Continental,100%,
+Regional,Regional,0%,
+Regional,National,0%,
+Regional,Continental,0%,
+Regional,National,≤15%,
+Regional,Continental,≤15%,
+Regional,National,≤30%,
+Regional,Continental,≤30%,
+National,National,0%,
+National,Continental,0%,
+National,Continental,≤15%,
+National,Continental,≤30%,
+Continental,Continental,0%,
 """
 
 AUTARKY_LEVEL_MAP = {
-    "100": "100%",
-    "85": "≥85%",
-    "70": "≥70%"
+    "100": "0%",
+    "85": "≤15%",
+    "70": "≤30%"
 }
 
 
@@ -44,7 +47,7 @@ def plot_costs(path_to_aggregated_results, path_to_costs):
     sns.set_context("paper")
     fig = plt.figure(figsize=(8, 3))
     pal = sns.light_palette(RED)
-    gs = gridspec.GridSpec(1, 4, width_ratios=[3, 2, 1, 0.2])
+    gs = gridspec.GridSpec(1, 4, width_ratios=[3, 2, 1, 0.1])
     ax4 = fig.add_subplot(gs[3])
     ax1 = fig.add_subplot(gs[0])
     sns.heatmap(
@@ -52,16 +55,17 @@ def plot_costs(path_to_aggregated_results, path_to_costs):
         cmap=pal,
         ax=ax1,
         cbar=False,
-        cbar_kws={"label": "total system costs"},
         vmin=results["cost"].min(),
         vmax=results["cost"].max(),
         linewidth=0.75,
         annot=True,
         fmt='.3g'
     )
+    plt.xlabel("")
+    ax1.tick_params(axis="x", labelcolor=AUTARKY_EXTENT_COLOR, labelrotation=0)
     ax1.annotate('a', xy=[-0.1, 1.05], xycoords='axes fraction',
                  fontsize=PANEL_FONT_SIZE, weight=PANEL_FONT_WEIGHT)
-    ax1.set_title("CONTINENTAL", loc="left")
+    ax1.set_title("Continental", loc="left", color=SYSTEM_SCALE_COLOR)
 
     ax2 = fig.add_subplot(gs[1])
     sns.heatmap(
@@ -76,10 +80,12 @@ def plot_costs(path_to_aggregated_results, path_to_costs):
         fmt='.3g'
     )
     plt.ylabel("")
+    plt.xlabel("")
     plt.yticks([])
+    ax2.tick_params(axis="x", labelcolor=AUTARKY_EXTENT_COLOR, labelrotation=0)
     ax2.annotate('b', xy=[-0.1 * 3 / 2, 1.05], xycoords='axes fraction',
                  fontsize=PANEL_FONT_SIZE, weight=PANEL_FONT_WEIGHT)
-    ax2.set_title("NATIONAL", loc="left")
+    ax2.set_title("National", loc="left", color=SYSTEM_SCALE_COLOR)
 
     ax3 = fig.add_subplot(gs[2])
     sns.heatmap(
@@ -87,6 +93,7 @@ def plot_costs(path_to_aggregated_results, path_to_costs):
         cmap=pal,
         ax=ax3,
         cbar_ax=ax4,
+        cbar_kws={"label": "Cost relative to lowest cost\n continental scale system"},
         vmin=results["cost"].min(),
         vmax=results["cost"].max(),
         linewidth=0.75,
@@ -94,17 +101,36 @@ def plot_costs(path_to_aggregated_results, path_to_costs):
         fmt='.3g'
     )
     plt.ylabel("")
+    plt.xlabel("")
     plt.yticks([])
+    ax3.tick_params(axis="x", labelcolor=AUTARKY_EXTENT_COLOR, labelrotation=0)
     ax3.annotate('c', xy=[-0.1 * 3, 1.05], xycoords='axes fraction',
                  fontsize=PANEL_FONT_SIZE, weight=PANEL_FONT_WEIGHT)
-    ax3.set_title("REGIONAL", loc="left")
+    ax3.set_title("Regional", loc="left", color=SYSTEM_SCALE_COLOR)
 
     for color, ax in zip(HIGHLIGHT_COLORS, fig.axes[1:]): # highlight base cases
         ax.add_patch(Rectangle((0.04, 2.04), 0.92, 0.92, fill=False, edgecolor=color, lw=HIGHLIGHT_LINEWIDTH))
 
-    fig.autofmt_xdate()
-    plt.tight_layout()
-    plt.subplots_adjust(wspace=0.6)
+    fig.text(
+        s="──────────────────────────────── System scale ────────────────────────────────",
+        y=0.98,
+        x=0.45,
+        color=SYSTEM_SCALE_COLOR,
+        horizontalalignment="center",
+        verticalalignment="top",
+        weight="bold"
+    )
+    fig.text(
+        s="──────────────────────────── Self-sufficiency extent ───────────────────────────",
+        y=0.02,
+        x=0.45,
+        color=AUTARKY_EXTENT_COLOR,
+        horizontalalignment="center",
+        verticalalignment="bottom",
+        weight="bold"
+    )
+    fig.tight_layout()
+    plt.subplots_adjust(wspace=0.6, top=0.82, bottom=0.2)
     fig.savefig(path_to_costs, dpi=600, transparent=False)
 
 
@@ -126,8 +152,8 @@ def cost_heatmap(data, grid_scale):
         values="cost"
     )
     pivoted_data.columns.name = "Self-sufficiency layer"
-    pivoted_data.index.name = "Self-sufficiency degree"
-    return pivoted_data[columns].reindex(["≥70%", "≥85%", "100%"])
+    pivoted_data.index.name = "Allowed net imports"
+    return pivoted_data[columns].reindex(["≤30%", "≤15%", "0%"])
 
 
 def read_results(path_to_agregrated_results):
